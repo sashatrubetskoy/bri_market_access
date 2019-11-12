32b4d559e18a90d1c942731dcdf2e619d53f8a19
sashatrubetskoy <trub@uchicago.edu>
2019-11-12 08:30:22 -0500

    python3

--- install_packages.sh
+++ install_packages.sh
@@ -5,16 +5,16 @@
 # trub@uchicago.edu
 
 echo "Installing packages..."
-pip install --force-reinstall numpy==1.14.2
-pip install --force-reinstall pandas==0.24.1
-pip install --force-reinstall xlrd==1.1.0
-pip install --force-reinstall unidecode==1.0.22
-pip install --force-reinstall geopy==1.14.0
-pip install --force-reinstall scipy==1.0.1
-pip install --force-reinstall networkx==2.1
-pip install --force-reinstall tqdm==4.31.1
-pip install --force-reinstall pyproj==1.9.5.1
-pip install --force-reinstall seaborn==0.9.0
+pip3 install --force-reinstall numpy==1.14.2
+pip3 install --force-reinstall pandas==0.24.1
+pip3 install --force-reinstall xlrd==1.1.0
+pip3 install --force-reinstall unidecode==1.0.22
+pip3 install --force-reinstall geopy==1.14.0
+pip3 install --force-reinstall scipy==1.0.1
+pip3 install --force-reinstall networkx==2.1
+pip3 install --force-reinstall tqdm==4.31.1
+pip3 install --force-reinstall pyproj==1.9.5.1
+pip3 install --force-reinstall seaborn==0.9.0
 
 echo "Necessary packages installed."
 echo "Done."
--- step_3.sh
+++ step_3.sh
@@ -5,13 +5,13 @@
 # trub@uchicago.edu
 
 echo "Running all projects..." # 50 hours
-python code/analysis/run_all_projects.py
+python3 code/analysis/run_all_projects.py
 echo "Done running all projects."
 
 echo "Creating top project for cities table..."
-python code/tables_and_figures/table_top_project_for_cities.py
+python3 code/tables_and_figures/table_top_project_for_cities.py
 echo "Top project for cities table created."
 
 echo "Creating top projects table..."
-python code/tables_and_figures/table_top_projects.py
+python3 code/tables_and_figures/table_top_projects.py
 echo "Top projects table created."
